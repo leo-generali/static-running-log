6757fb3c05835ee26ae7fca1bccb2b1b92532e61
Leo Generali <leogen17@gmail.com>
2020-03-15 13:47:48 -0400

    Minimize CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,18 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/site/**/*.njk", "./src/site/**/*.md"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+const cssnano = require("cssnano");
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    require("postcss-nested")
+    require("postcss-nested"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : [])
   ]
 };
